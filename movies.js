1e22ae10de04944c488eeebd5c67cb7e1c88baa2
MovieHD99 <utpolray8350@gmail.com>
2025-06-08 15:14:49 +0600

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,317 @@
+const movies = [
+    {
+        "title": "Bachelor Point Season 5 Ep 4",
+        "year": "2025",
+        "category": "Romance",
+        "img": "https://cdn.bongo-solutions.com/abfea462-f64d-491e-9cd9-75ee001f45b0/content/d961fe4c-1ae8-4f8f-ac91-e009a3a32391/5060d765-fd71-4493-8969-3fff61ddf11f.jpg",
+        "desc": "Full in Bangla WEB-DL",
+        "download": "#",
+        "videoUrl": "https://rumble.com/embed/v6s92xv/?pub=1tf892"
+    },
+    {
+        "title": "Bachelor Point Season 5 Ep 3",
+        "year": "2025",
+        "category": "Romance",
+        "img": "https://cdn.bongo-solutions.com/abfea462-f64d-491e-9cd9-75ee001f45b0/content/d961fe4c-1ae8-4f8f-ac91-e009a3a32391/5060d765-fd71-4493-8969-3fff61ddf11f.jpg",
+        "desc": "Full in Bangla WEB-DL",
+        "download": "#",
+        "videoUrl": "https://rumble.com/embed/v6s93jt/?pub=1tf892"
+    },
+    {
+        "title": "Bachelor Point Season 5 Ep 2",
+        "year": "2025",
+        "category": "Romance",
+        "img": "https://cdn.bongo-solutions.com/abfea462-f64d-491e-9cd9-75ee001f45b0/content/d961fe4c-1ae8-4f8f-ac91-e009a3a32391/5060d765-fd71-4493-8969-3fff61ddf11f.jpg",
+        "desc": "Full in Bangla WEB-DL",
+        "download": "#",
+        "videoUrl": "https://rumble.com/embed/v6s93nj/?pub=1tf892"
+    },
+    {
+        "title": "Bachelor Point Season 5 Ep 1",
+        "year": "2025",
+        "category": "Romance",
+        "img": "https://cdn.bongo-solutions.com/abfea462-f64d-491e-9cd9-75ee001f45b0/content/d961fe4c-1ae8-4f8f-ac91-e009a3a32391/5060d765-fd71-4493-8969-3fff61ddf11f.jpg",
+        "desc": "Full in Bangla WEB-DL",
+        "download": "#",
+        "videoUrl": "https://rumble.com/embed/v6s92fr/?pub=1tf892"
+    },
+    {
+        "title": "Borbaad",
+        "year": "2025",
+        "category": "Action, Romance, Thriller",
+        "img": "https://i.ibb.co/ZpsRKJhK/1749156497418.jpg",
+        "desc": "Full HD Movie in Bangla WEB-DL",
+        "download": "#",
+        "videoUrl": "https://vembed.online/e/tt33305312"
+    },
+    {
+        "title": "Chokkor 302",
+        "year": "2025",
+        "category": "Mystery, Thriller",
+        "img": "https://image.tmdb.org/t/p/original/o2oOpfhEKmKxlmZr0DBsIhizMhg.jpg",
+        "desc": "Full HD movie in Bangla WEB-DL",
+        "download": "#",
+        "videoUrl": "https://vembed.online/e/tt36108604"
+    },
+    {
+        "title": "Daagi",
+        "year": "2025",
+        "category": "Action, Romance",
+        "img": "https://m.media-amazon.com/images/M/MV5BNGRlNWFmNmItZjFkMy00OGRkLWEyMzUtNDZlMDIxMjRlN2M2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        "desc": "Full HD Movie in Bangla WEB-DL",
+        "download": "#",
+        "videoUrl": "https://vembed.online/e/tt34936199"
+    },
+    {
+        "title": "Thug Life",
+        "year": "2025",
+        "category": "Action, Drama, Thriller",
+        "img": "https://img.imageride.net/images/2025/06/05/05_06_2025_P_192.jpg",
+        "desc": "HDTS Best Quality Print Full Movie in Hindi S-Print",
+        "download": "#",
+        "videoUrl": "https://vembed.online/e/tt23398540"
+    },
+    {
+        "title": "Tourist Family",
+        "year": "2025",
+        "category": "Comedy, Drama",
+        "img": "https://m.media-amazon.com/images/M/MV5BM2NlZTI0NzEtMTRjYi00YjA2LWJmMzEtMjkzMWNlZWQ4NTZlXkEyXkFqcGc@._V1_.jpg",
+        "desc": "Full HD Hindi ORG Movie in Malayalam Multi Audio 4K uHDRip",
+        "download": "/go2link/c-y_BWVovmsZ9-mdpGl5X1SxTOQILEO137S8z8X484g4TFup5Lkbh96aj9Y3TyibABe_ORmydS-DB3v_j8ywFw",
+        "videoUrl": "https://vembed.online/e/tt34915705"
+    },
+    {
+        "title": "840 (2024) S01 Bengali HDTVRip x264 AAC 720p",
+        "year": "2024",
+        "category": "Bangla",
+        "img": "https://m.media-amazon.com/images/M/MV5BYzA0ZWM3MWQtZGNiOS00MDM1LTkyMGMtOTQyN2ZlOTRmZTAyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        "desc": "A WEB-DL comedy-crime-drama movie in Bangla.",
+        "download": "https://drive.google.com/file/d/1ixO2dljCB8BisXb4w7bg3qdyEJMDuM4Z/view?usp=sharing",
+        "videoUrl": "https://youtube.googleapis.com/embed/?autohide=1&ps=docs&enablecastapi=0&hl=en-GB&partnerid=30&post_message_origin=https%3A%2F%2Fdrive.google.com&origin=https%3A%2F%2Fdrive.google.com&hbenv=apps-elements&enablejsapi=1"
+    },
+    {
+        "title": "Mirza-Chapter: Club 29",
+        "year": "2025",
+        "category": "Bangla",
+        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThQgrG8ZVbeeoWK5_LV9lHsM2oN4bQz7ON_Cf_S_EaQ__bgZwjs8QmhuaV&s=10",
+        "desc": "A WEB-DL comedy-crime-drama movie in Bangla.",
+        "download": "/go2link/uXcHVnZWbK_-6Tyahqxr6oQci3Xtryza-_2VPUVQxWoEZyCXMDye5jsDNAz8adNaFVhYlPu1DbpKp39Hkkr1G2w6RgZgvuDH0vb5ycFh7RU",
+        "videoUrl": "https://vembed.online/e/tt36955562"
+    },
+    {
+        "title": "Captain America: Brave New World",
+        "year": "2025",
+        "category": "Hollywood",
+        "img": "https://preview.redd.it/official-poster-for-captain-america-brave-new-world-v0-jkgmtnwszxzd1.jpeg?auto=webp&s=90b1ec0b517d9fcc07bfd5b06c8da5218d574c35",
+        "desc": "A Full HD Hindi ORG action-adventure sci-fi movie available in Blu-Ray with Hindi, English, and Multi Audio options.",
+        "download": "/go2link/l-Nupq4OKahYsz24W7TiXx1GmnBQFcxfkKS9saL1x7u-ee75sO24LvzGyVwKky634cIt_q8PFjdOzqmE3XvgglZk7I927iO54u1sMXcko13QDiMnc5TCbXhOinka78uY",
+        "videoUrl": "https://vembed.online/e/tt14513804"
+    },
+    {
+        "title": "Thudarum",
+        "year": "2025",
+        "category": "Bollywood",
+        "img": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/ipumuu0FF9yr3pIaqGBaAAYCu23.jpg",
+        "desc": "A Full HD Hindi ORG drama movie available in 4K uHDRip with Hindi, Malayalam, and Multi Audio options.",
+        "download": "/go2link/iYet4XKQkNe4MIdg0HjeHa014mRLWoC2Pu4ZQkThfss3oCzHXKdsl_l03_6ktbHL4A8-hZitljwAJHSQpeaxgQ",
+        "videoUrl": "https://vembed.online/e/tt31969600"
+    },
+    {
+        "title": "Ong Bak 2: The Beginning",
+        "year": "2008",
+        "category": "Hollywood",
+        "img": "https://www.themoviedb.org/t/p/original/2yb5KUuojISi0iYrDX5VxExJILM.jpg",
+        "desc": "An action-packed adventure movie available in Hindi and Dual Audio in WEB-DL quality.",
+        "download": "/go2link/KdK0AN_bRmKq5VwGt7BwslBP4TniiU6I9vVRtRiDTBgIE6Qjil0gurA1AZwdKuSii-FYTqHFXHnHAZzj0mjWpWKNKKpVL1GQxOS8SN3JfX4",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Retro",
+        "year": "2025",
+        "category": "Bollywood",
+        "img": "https://m.media-amazon.com/images/M/MV5BYzZjYzU0OGMtODI1OS00MjU5LThlOTAtZjY2NmZlOTVlMmE5XkEyXkFqcGc@._V1_.jpg",
+        "desc": "A Full HD Hindi ORG movie in Action, Drama, and Romance genres, available in 4K uHDRip with Hindi, Tamil, and Multi Audio options.",
+        "download": "/go2link/RfSBlfPV2hc76Ltgac-uTKoGiVIPoFpnyttN0JyA-hHfZ7YbtxNjJvUXRMJV84lGbk_8hGWjqjpLpzhvJzBzkw",
+        "videoUrl": "https://vembed.online/e/tt31945132"
+    },
+    {
+        "title": "Ondhokarer Gaan (2025)",
+        "year": "2025",
+        "category": "Bangla",
+        "img": "https://image.tmdb.org/t/p/original/5zlHKvMt5iwhtSRXy1td7n4WAwW.jpg",
+        "desc": "This is a great Bangla movie released in 2025.",
+        "download": "#",
+        "videoUrl": "https://vembed.online/e/tt35317601"
+    },
+    {
+        "title": "HIT: The 3rd Case (2025)",
+        "year": "2025",
+        "category": "Bollywood",
+        "img": "https://m.media-amazon.com/images/M/MV5BNTI0Yzc1Y2EtNDg4MS00OWJmLWI4ODItMjFjMDQ4NjNjM2NhXkEyXkFqcGc@._V1_.jpg",
+        "desc": "This is a great Bollywood movie released in 2025.",
+        "download": "https://m.movielinkbd.work/go2link/TZjmDC2uX7JZjLGcESbKIyffCaAHj9QeC0K2rX9kgmz1jeZ7OhUYN91-XdpsL8RXRIJb0hE_igLPu5F6NjOlqn2Ghf_5iCI5cnVzaX_Nw60",
+        "videoUrl": "https://vembed.online/e/tt23804696"
+    },
+    {
+        "title": "The Bhootnii (2025) Hindi Movie",
+        "year": "2025",
+        "desc": "The Bhootnii (2025) Hindi Movie",
+        "category": "Bollywood",
+        "img": "https://www.filmyzilla11.com/files/images/The-bhootnii-(2025).jpg?time=1748001613",
+        "download": "https://www.filmyzilla11.com/downloads/29933/1/server_1/",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Kesari Chapter 2 The Untold Story of Jallianwala Bagh (2025) Hindi Movie",
+        "year": "2025",
+        "desc": "Kesari Chapter 2 The Untold Story of Jallianwala Bagh (2025) Hindi Movie",
+        "category": "Bollywood",
+        "img": "https://www.filmyzilla11.com/files/images/Kesari-chapter-2-the-untold-story-of-jallianwala-bagh-(2025)-hindi-movie.jpg?time=1748001645",
+        "download": "https://www.filmyzilla11.com/server/29495/Kesari-Chapter-2-The-Untold-Story-Of-Jallianwala-Bagh-(2025)-Hindi-Movie-1080p-HDTC.mkv.html",
+        "videoUrl": "#"
+    },
+    {
+        "title": "REC Apocalypse",
+        "year": "2022",
+        "desc": "REC Apocalypse",
+        "category": "Hollywood",
+        "img": "https://image.tmdb.org/t/p/original/nlSSZNESHkY1iPzYnpcRvYUr7Mz.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Jongli 2025 Full HD Movie",
+        "year": "2025",
+        "desc": "A troubled university student, haunted by family trauma and heartbreak, descends into isolation--until an unexpected bond with a little girl forces him to confront his pain and choose between redemption or remaining lost. Director: Iam Ahmed, Noireeta Hasin, Roudromoyee, Shabnam Bubl",
+        "category": "Bangla",
+        "img": "https://tds-images.thedailystar.net/sites/default/files/styles/medium_204/public/images/2024/05/28/jongli_poster.jpg",
+        "download": "https://t.me/c/2198234076/212",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Daagi 2025 Full HD Movie",
+        "year": "2025",
+        "desc": "Despite his father's advice to work honestly, middle-class Nishan rejects steady employment and seeks quick wealth through risky means.",
+        "category": "Bangla",
+        "img": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Daagi_%282025_film%29.jpg/250px-Daagi_%282025_film%29.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Rajbanshi Comedy: Jamai Shoshur 420",
+        "year": "2024",
+        "desc": "A hilarious Rajbanshi comedy skit about the funny antics between a son-in-law and father-in-law, filled with local humor and cultural references.",
+        "category": "Bangla",
+        "img": "https://i.imgur.com/placeholder.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Bhavani Ward 1997 2025 Full HD Movie",
+        "year": "2025",
+        "desc": "Bhavani Ward 1997 2025 Full HD Movie",
+        "category": "Hollywood",
+        "img": "https://images2.imgbox.com/e3/cf/GZRzzpVb_o.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "The Batman",
+        "year": "2022",
+        "desc": "Batman ventures into Gotham’s underworld.",
+        "category": "Hollywood",
+        "img": "https://i.imgur.com/Zi6v09X.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Oppenheimer",
+        "year": "2023",
+        "desc": "The story of J. Robert Oppenheimer.",
+        "category": "Hollywood",
+        "img": "https://i.imgur.com/FkJYkAI.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "RRR",
+        "year": "2022",
+        "desc": "Two revolutionaries fight for their country.",
+        "category": "Bollywood",
+        "img": "https://i.imgur.com/4M7DG9c.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "KGF Chapter 2",
+        "year": "2022",
+        "desc": "Rocky fights oppression at KGF.",
+        "category": "Bollywood",
+        "img": "https://i.imgur.com/q9RAmnh.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Mirzapur",
+        "year": "2018",
+        "desc": "A crime thriller in India.",
+        "category": "Bangla",
+        "img": "https://i.imgur.com/EkKtZzB.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Pushpa",
+        "year": "2021",
+        "desc": "A laborer rises in a smuggling syndicate.",
+        "category": "Bollywood",
+        "img": "https://i.imgur.com/XlRfrOl.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Inception",
+        "year": "2010",
+        "desc": "A thief who steals secrets via dreams.",
+        "category": "Hollywood",
+        "img": "https://i.imgur.com/Kh9O5AQ.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Dune",
+        "year": "2021",
+        "desc": "Paul Atreides fights for planet Arrakis.",
+        "category": "Hollywood",
+        "img": "https://i.imgur.com/V5POslk.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "The Dark Knight",
+        "year": "2008",
+        "desc": "Batman fights Gotham's criminal underworld.",
+        "category": "Hollywood",
+        "img": "https://i.imgur.com/D2CdrBa.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Jawan",
+        "year": "2023",
+        "desc": "A man exposes corruption in society.",
+        "category": "Bollywood",
+        "img": "https://i.imgur.com/CqV8FZP.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    },
+    {
+        "title": "Spider-Man: No Way Home",
+        "year": "2021",
+        "desc": "Peter Parker seeks help from Doctor Strange.",
+        "category": "Hollywood",
+        "img": "https://i.imgur.com/4bmrrA4.jpg",
+        "download": "#",
+        "videoUrl": "#"
+    }
+];
